1bdb40a2a751892aa460dd038feaf28e43d73a6c
MateusLSants <mateusleandro23@gmail.com>
2023-01-27 13:49:44 -0300

    Adicionado entidade 'Person' e adicinando classe da validação

---
+++ src/Backend/AppComprasNetCore.Domain/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace AppComprasNetCore.Domain.Entities.Person;
+
+public sealed class Person
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Document { get; private set; }
+    public string Phone { get; private set; }
+
+    public Person(string nome, string document, string phone)
+    {
+    }
+    
+    private void Validation(string nome, string document, string phone)
+    {
+        DomainValidation
+    }
+}
---
+++ src/Backend/AppComprasNetCore.Domain/Validations/DomaniValidationException.cs
@@ -0,0 +1,15 @@
+﻿namespace AppComprasNetCore.Domain.Validations;
+
+internal class DomaniValidationException : Exception
+{
+	public DomaniValidationException(string error) : base()
+	{ }
+
+	public static void When(bool hasError , string message)
+	{
+		if (hasError)
+		{
+			throw new DomaniValidationException(message);
+		}
+	}
+}
