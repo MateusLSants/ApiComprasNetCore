a57d4938ffc2e48a83b904dd0949b7c2da39332c
MateusLSants <mateusleandro23@gmail.com>
2023-01-27 14:05:37 -0300

    Adicionado a entidade de produto e validações desta

--- src/Backend/AppComprasNetCore.Domain/Entities/Person.cs
+++ src/Backend/AppComprasNetCore.Domain/Entities/Person.cs
@@ -1,4 +1,6 @@
-﻿namespace AppComprasNetCore.Domain.Entities.Person;
+﻿using AppComprasNetCore.Domain.Validations;
+
+namespace AppComprasNetCore.Domain.Entities.Person;
 
 public sealed class Person
 {
@@ -7,12 +9,26 @@ public sealed class Person
     public string Document { get; private set; }
     public string Phone { get; private set; }
 
-    public Person(string nome, string document, string phone)
+    public Person(string name, string document, string phone)
     {
+        Validation(name, document, phone);
+    }
+
+    public Person(int id, string name, string document, string phone) 
+    {
+        DomaniValidationException.When(id < 0, "Id deve ser maior que zero");
+        Id = id;
+        Validation(name, document, phone);
     }
     
-    private void Validation(string nome, string document, string phone)
+    private void Validation(string name, string document, string phone)
     {
-        DomainValidation
+        DomaniValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado!");
+        DomaniValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informado!");
+        DomaniValidationException.When(string.IsNullOrEmpty(phone), "Telefone deve ser informado!");
+
+        Name = name;
+        Document = document;
+        Phone = phone;
     }
 }
