0d7f3f22b11957490822cbac3409a12f898d8898
MateusLSants <mateusleandro23@gmail.com>
2023-01-28 11:54:15 -0300

    Adicionado a classe de compras

---
+++ src/Backend/AppComprasNetCore.Domain/Entities/Purchase.cs
@@ -0,0 +1,18 @@
+﻿namespace AppComprasNetCore.Domain.Entities;
+
+
+public sealed class Purchase
+{
+    public int Id { get; private set; }
+    public int ProductId { get; private set; }
+    public int PersonId { get; private set; }
+    public DateTime Date { get; private set; }
+    public Person Person { get; set; }
+
+    public Purchase()
+    {
+
+    }
+}
+
+
