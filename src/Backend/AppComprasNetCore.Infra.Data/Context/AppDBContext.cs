c9d14c99e285c993ce37d1e373618463e674ef5c
MateusLSants <mateusleandro23@gmail.com>
2023-01-28 13:20:48 -0300

    Iniciando classe de banco de dados e DBcontext

---
+++ src/Backend/AppComprasNetCore.Infra.Data/Context/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppComprasNetCore.Infra.Data.Context;
+
+public class AppDBContext : DbContext
+{
+
+	public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+	{ }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDBContext).Assembly);
+    }
+}
---
+++ src/Backend/AppComprasNetCore.Infra.Data/Migrations/DataBase.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace AppComprasNetCore.Infra.Data.Migrations;
+
+public static class DataBase
+{
+    public static void CriarDataBase(string conexaoBancoDados, string nomeBancoDados)
+    {
+        using var minhaConexao = new MySqlConnection(conexaoBancoDados);
+
+        var parametros = new DynamicParameters();
+        parametros.Add("nome", nomeBancoDados);
+
+        var registros = minhaConexao.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @nome", parametros);
+
+        if (!registros.Any())
+        {
+            minhaConexao.Execute($"CREATE DATABASE {nomeBancoDados}");
+        }
+    }    
+}
